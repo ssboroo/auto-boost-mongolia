4d6a1e8da061fc3f790a0f4cdcdf601e99727e89
ssboroo <89896772+ssboroo@users.noreply.github.com>
2026-09-03 02:05:17 +0800

    feat: register tenant token vault service

--- backend/src/meta/meta.module.ts
+++ backend/src/meta/meta.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common'
 import { MetaController } from './meta.controller'
 import { MetaService } from './meta.service'
+import { TenantService } from './tenant.service'
 
 @Module({
   controllers: [MetaController],
-  providers: [MetaService],
-  exports: [MetaService],
+  providers: [MetaService, TenantService],
+  exports: [MetaService, TenantService],
 })
 export class MetaModule {}
