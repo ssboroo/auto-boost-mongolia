b8fcee9062d3ee924f7681e64d6486a1ee233c0b
ssboroo <89896772+ssboroo@users.noreply.github.com>
2026-09-02 22:26:58 +0800

    feat: wire Windsor module into backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
+import { WindsorModule } from './windsor/windsor.module'
 
 @Module({
+  imports: [WindsorModule],
   controllers: [AppController],
 })
 export class AppModule {}
