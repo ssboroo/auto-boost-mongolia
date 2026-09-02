bfb6fad4886e688206a1bedab0e60da52771b44d
ssboroo <89896772+ssboroo@users.noreply.github.com>
2026-09-03 04:04:47 +0800

    feat: register Meta readiness service

--- backend/src/meta/meta.module.ts
+++ backend/src/meta/meta.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common'
 import { MetaController } from './meta.controller'
+import { MetaReadinessController } from './meta-readiness.controller'
+import { MetaReadinessService } from './meta-readiness.service'
 import { MetaService } from './meta.service'
 import { TenantService } from './tenant.service'
 
 @Module({
-  controllers: [MetaController],
-  providers: [MetaService, TenantService],
-  exports: [MetaService, TenantService],
+  controllers: [MetaController, MetaReadinessController],
+  providers: [MetaService, MetaReadinessService, TenantService],
+  exports: [MetaService, MetaReadinessService, TenantService],
 })
 export class MetaModule {}
